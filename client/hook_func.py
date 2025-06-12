1e748cb12cb1b0611737d58aa877aba3ae36801a
hkbin <hkbinbinbin@gmail.com>
2025-06-12 09:24:30 +0800

    need fix arg localtion bug

--- client/hook_func.py
+++ client/hook_func.py
@@ -7,14 +7,15 @@ old_var_types = {}
 
 class FunctionHexrayHooks(ida_hexrays.Hexrays_Hooks):
     # store var old type
-    def func_printed(self, vu):
-        ea = vu.entry_ea
+    # func_printed() => cfunc->get_lvars()
+    def func_printed(self, cfunc):
+        ea = cfunc.entry_ea
         if ea not in funcs_table:
             funcs_table.add(ea) # add address
-            lvars = vu.get_lvars()
-            value = set()
+            lvars = cfunc.get_lvars()
+            value = []
             for lv in lvars:
-                value.add((lv.name, lv.tif.copy().dstr())) # store (name, type)
+                value.append((lv.name, lv.tif.copy().dstr())) #store (name, type, location)
             old_var_types[ea] = value
             print(f"[Hook] cache: {old_var_types}")
         return 0
@@ -27,43 +28,41 @@ class FunctionHexrayHooks(ida_hexrays.Hexrays_Hooks):
         if cached:
             # find type from set
             old_type = None
-            for var_name, var_tinfo in cached:
+            for i, (var_name, var_tinfo) in enumerate(cached):
                 if var_name == v.name:
                     old_type = var_tinfo
+                    print(f"[Hook] In function addr: {vu.cfunc.entry_ea}")
+                    print(f"[Hook] Var name: {v.name}")
+                    print(f"[Hook] Changed var width: {v.width}")
+                    print(f"[Hook] Var Old type: {old_type}")
+                    print(f"[Hook] Var New type: {tinfo.dstr()}")
+                    cached[i] = (v.name, tinfo.dstr()) # update
                     break
-            if old_type == None:
-                # case change from server
-                return
-            print(f"[Hook] In function addr: {vu.cfunc.entry_ea}")
-            print(f"[Hook] Var name: {v.name}")
-            print(f"[Hook] Changed var width: {v.width}")
-            print(f"[Hook] Var Old type: {old_type}")
-            print(f"[Hook] Var New type: {tinfo.dstr()}")
-            old_var_types[key] = (v.name, tinfo.dstr())
-        else:
-            # case change from server
-            print(f"[Hook] Var name: {v.name} - no old type cached")
+        # case change from server
+        print(f"[Hook] Var name: {v.name} - no old type cached")
 
     # func var name change hook
     def lvar_name_changed(self, vu, v, name, is_user_name):
         key = vu.cfunc.entry_ea
         cached = old_var_types.get(key, None)
         if cached:
+            cfunc = vu.cfunc
+            lvars = cfunc.get_lvars()
+            new_value = []
+            for lv in lvars:
+                value.append((lv.name, lv.tif.copy().dstr())) #store (name, type, location)
             old_name = None
-            for var_name, var_tinfo in cached:
-                if var_tinfo == v.tif.dstr():
-                    old_name = var_name
-                    break
-            if old_name == None:
-                # case change from server
-                return
-            old_var_types[key] = (name, v.tif.dstr())
-            print(f"[Hook] In function addr: {vu.cfunc.entry_ea}")
-            print(f"[Hook] Old Var name: {old_name}")
-            print(f"[Hook] New Var name: {name}")
-        else:
-            # case change from server
-            print(f"[Hook] no old name cached")
+            for i, (var_name, var_tinfo) in enumerate(cached):
+                
+                # if (v.location.compare(var_location)): #TODO store (name, type, location) need better method to distinguish variables
+                #     old_name = var_name
+                #     cached[i] = (name, v.tif.dstr(), v)
+                #     print(f"[Hook] In function addr: {key}")
+                #     print(f"[Hook] Old Var name: {old_name}")
+                #     print(f"[Hook] New Var name: {name}")
+                #     return
+        # case change from server
+        print(f"[Hook] no old name cached")
 
 
 function_hook = FunctionHexrayHooks()
